ee7c74ce35852d363f94334e4437dae43433227b
ZABILA PETRO <petrozabila@gmail.com>
2022-09-28 16:41:12 +0300

    loader fixes

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,12 +1,8 @@
 import { Audio } from 'react-loader-spinner';
-import styles from './loader.module.scss';
 
 export default function Loader () {
     return (
         <>
-            <div className={styles.backdrop}>
-                <div className={styles.loader}></div>
-            </div>
             <Audio
                 height="80"
                 width="80"
